d885f6dc6564a53115a3164e4ca3072f3f83db61
Noor Mohammed <noormohammedb@protonmail.com>
2022-12-06 11:49:24 +0400

    Completed if and quiz

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(no_of_apples: u32) -> u32 {
+    if no_of_apples > 40 {
+        no_of_apples * 1
+    } else {
+        no_of_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
